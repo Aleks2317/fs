7be5b49b6c668dc94f3c5e9dd8596f090c8422f3
Aleksei Kavko <108331145+Aleks2317@users.noreply.github.com>
2024-06-18 19:21:02 +0300

    настройка админки

--- funny_stories_project/game/admin.py
+++ funny_stories_project/game/admin.py
@@ -11,9 +11,25 @@ class UsersAdmin(admin.ModelAdmin):
     ordering = ['age', '-data_registration']
     # добавление фильтрации в список изменения
     list_filter = ['data_registration', 'name']
+    search_fields = ['name']
+    search_help_text = 'Поиск по полю name'
 
 
-admin.site.register(Games)
+@admin.action(description="Поставить status = True")
+def reset_status(modeladmin, request, queryset):
+    queryset.update(status=True)
+
+
+class GamesAdmin(admin.ModelAdmin):
+    list_display = ['title_story', 'list_players', 'story', 'data_game', 'status']
+    list_filter = ['status', 'data_game', 'title_story']
+    ordering = ['-data_game']
+    search_fields = ['title_story']
+    search_help_text = 'Поиск по полю title_story'
+    actions = [reset_status]
+
+
+admin.site.register(Games, GamesAdmin)
 admin.site.register(Users, UsersAdmin)
 
 
